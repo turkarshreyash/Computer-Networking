0806ec51711641dbdd99acc4771224019e3941da
Shreyash7478 <shturkar90@gmail.com>
2019-08-05 10:55:38 +0530

    Lab3 Serve-Python

---
+++ Lab3/Server.py
@@ -0,0 +1,22 @@
+import socket
+
+port_number = 2328
+
+
+if __name__ == "__main__":
+    s = socket.socket()
+    print("Socket Created !")
+    s.bind(('',port_number))
+    print("Bind Exec !")
+    s.listen(5)
+    while True:
+        print("Listening !")
+        client, client_addr = s.accept()
+        print(f"{client_addr} is requesting connection !")
+        while True:
+            if input("Want send message ? ") == "0":
+                break
+            message = input("Type message : ")
+            client.send(message.encode()) 
+            print(f"{client.recv(1024).decode()}")
+        client.close()
